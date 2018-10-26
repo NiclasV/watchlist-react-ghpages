4b5a1bf480994cc6319f5df830a435902ddc596d
NiclasV <niclas.victorsson@medieinstitutet.se>
2018-10-26 14:33:30 +0200

    big things poppin

--- src/App.js
+++ src/App.js
@@ -1,28 +1,121 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+class Navigation extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <nav>
+        <Menu />
+        <Loginfield />
+      </nav>
+    );
+  }
+
+}
+class Menu extends Component {
+
+  render() {
+    return (
+      <div>
+        MENU
       </div>
     );
   }
+
 }
+
+class Loginfield extends Component {
+
+  render() {
+    return (
+      <div>:LOGIN:</div>
+    );
+  }
+
+}
+
+class Search extends Component {
+
+  render() {
+    return (
+      <div>Watchlists Bruh</div>
+    );
+  }
+
+}
+
+class SearchField extends Component {
+
+  render() {
+    return (
+      <form>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+      </form>
+    );
+  }
+
+}
+
+class Watchlists extends Component {
+
+  render() {
+    return (
+      <div>Watchlists Bruh</div>
+    );
+  }
+
+}
+
+class Movietips extends Component {
+
+  render() {
+    return (
+      <div>Movie Recommendations</div>
+    );
+  }
+
+}
+
+class Footer extends Component {
+
+  render() {
+    return (
+      <footer>That footer tho</footer>
+    );
+  }
+
+}
+
+class Logo extends Component {
+
+  render() {
+    return (
+      <div>A FOREIGN BRISK, THATS MY CUP OF TEA yea</div>
+    );
+  }
+
+}
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Watchlists />
+        <Movietips />
+        <Footer />
+      </div>
+    );
+  }
+
+}
+
 
 export default App;
