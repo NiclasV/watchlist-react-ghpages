250f73d2bd9ac99702b83a8b387016b0ab441198
NiclasV <niclas.victorsson@medieinstitutet.se>
2018-12-03 12:09:29 +0100

    wewe

--- src/App.js
+++ src/App.js
@@ -1,116 +1,13 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation';
 import './App.css';
-
-
-class Navigation extends Component {
-
-  render() {
-    return (
-      <nav>
-        <Menu />
-        <Loginfield />
-      </nav>
-    );
-  }
-
-}
-class Menu extends Component {
-
-  render() {
-    return (
-      <div>
-        MENU
-      </div>
-    );
-  }
-
-}
-
-class Loginfield extends Component {
-
-  render() {
-    return (
-      <div>:LOGIN:</div>
-    );
-  }
-
-}
-
-class Search extends Component {
-
-  render() {
-    return (
-      <div>Watchlists Bruh</div>
-    );
-  }
-
-}
-
-class SearchField extends Component {
-
-  render() {
-    return (
-      <form>
-        <div className="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-      </form>
-    );
-  }
-
-}
-
-class Watchlists extends Component {
-
-  render() {
-    return (
-      <div>Watchlists Bruh</div>
-    );
-  }
-
-}
-
-class Movietips extends Component {
-
-  render() {
-    return (
-      <div>Movie Recommendations</div>
-    );
-  }
-
-}
-
-class Footer extends Component {
-
-  render() {
-    return (
-      <footer>That footer tho</footer>
-    );
-  }
-
-}
-
-class Logo extends Component {
-
-  render() {
-    return (
-      <div>A FOREIGN BRISK, THATS MY CUP OF TEA yea</div>
-    );
-  }
-
-}
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Navigation />
-        <Watchlists />
-        <Movietips />
-        <Footer />
+        <Navigation/>
       </div>
     );
   }
---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+
+  render() {
+    return (
+        <div className="nav">
+            Nav
+        </div>
+    );  
+  }
+
+}
+
+
+export default Navigation;
