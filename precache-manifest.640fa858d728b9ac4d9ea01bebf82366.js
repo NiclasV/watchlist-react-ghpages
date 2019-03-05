ae4008768d34e0ee4268770060f59ccfd4994e0e
NiclasV <niclas.victorsson@medieinstitutet.se>
2019-03-05 17:13:33 +0100

    Updates

---
+++ precache-manifest.640fa858d728b9ac4d9ea01bebf82366.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c26010122f6210d7eaea13471a7d942e",
+    "url": "/static/media/recommended.c2601012.jpg"
+  },
+  {
+    "revision": "ef4d58c2c7127bb7a71c",
+    "url": "/static/css/main.14603908.chunk.css"
+  },
+  {
+    "revision": "9242e8644ff4620d2d22",
+    "url": "/static/js/1.9242e864.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ce9822cacdc3731fc4da7ad6d728855",
+    "url": "/static/media/bg-desk.5ce9822c.jpg"
+  },
+  {
+    "revision": "ef4d58c2c7127bb7a71c",
+    "url": "/static/js/main.ef4d58c2.chunk.js"
+  },
+  {
+    "revision": "0af76bfe25a0d8140f97a62cd12ee59e",
+    "url": "/static/media/logo.0af76bfe.png"
+  },
+  {
+    "revision": "b3ffcfc9304caef0b6fcc32694948cbe",
+    "url": "/static/media/footer_logo.b3ffcfc9.png"
+  },
+  {
+    "revision": "571fe20b4c9d6b7917909a4f87b95e5f",
+    "url": "/static/media/netflixbg2.571fe20b.jpg"
+  },
+  {
+    "revision": "ba54219c897238d7a623cd8380615a97",
+    "url": "/static/media/bgprofile.ba54219c.jpg"
+  },
+  {
+    "revision": "22f331312bb44d9b105efca42c45824d",
+    "url": "/index.html"
+  }
+];
