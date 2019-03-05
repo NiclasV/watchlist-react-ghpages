9c63587add153662df1c7d22e7d3724591f6c481
NiclasV <niclas.victorsson@medieinstitutet.se>
2019-03-05 17:23:37 +0100

    final

--- php/db.php
+++ php/db.php
@@ -1,10 +1,7 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: https://niclasv.github.io");
 header("content-type:application/json");
 $connect = mysqli_connect("localhost", "root", "root", "watchlist");
-
-
-
 
 ?>
